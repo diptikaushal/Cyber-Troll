647039af9d45c99f5302669aaf9996563e799dc8
diptikaushal <74426908+diptikaushal@users.noreply.github.com>
2021-09-17 16:54:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+nltk.download('stopwords')
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
